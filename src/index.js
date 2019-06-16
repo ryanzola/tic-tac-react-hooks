7110a01623986d097cb6f35425812c867dd41e6c
ryanzola <ryanzola@me.com>
2019-06-16 09:43:38 -0400

    Initial Commit - Converts example project from class-based to functional components using react hooks - Removes...pretty much everything - Includes a config.yaml for ci

--- src/index.js
+++ src/index.js
@@ -1,12 +1,74 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+function Square({value, onClick}) {
+  return (
+    <button 
+      className="square"
+      onClick={onClick}>
+      {value}
+    </button>
+  );
+}
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+function Board() {
+  const [squares, setSquares] = useState(Array(9).fill(null));
+  const [isXNext, setIsXNext] = useState(true);
+
+  function renderSquare(i) {
+    return <Square 
+      onClick={() => {
+        const nextSquare = squares.slice();
+        nextSquare[i] = isXNext ? 'x' : 'o'
+        console.log(nextSquare);
+        setSquares(nextSquare);
+        setIsXNext(!isXNext);
+        }
+      }
+      value={squares[i]}/>;
+  }
+
+  const status = 'Next player: X';
+
+  return (
+    <div>
+      <div className="status">{status}</div>
+      <div className="board-row">
+        {renderSquare(0)}
+        {renderSquare(1)}
+        {renderSquare(2)}
+      </div>
+      <div className="board-row">
+        {renderSquare(3)}
+        {renderSquare(4)}
+        {renderSquare(5)}
+      </div>
+      <div className="board-row">
+        {renderSquare(6)}
+        {renderSquare(7)}
+        {renderSquare(8)}
+      </div>
+    </div>
+  );
+}
+
+class Game extends React.Component {
+  render() {
+    return (
+      <div className="game">
+        <div className="game-board">
+          <Board />
+        </div>
+        <div className="game-info">
+          <div>{/* status */}</div>
+          <ol>{/* TODO */}</ol>
+        </div>
+      </div>
+    );
+  }
+}
+
+// ========================================
+
+ReactDOM.render(<Game />, document.getElementById('root'));
