87ddef49d6f451dc1c1f12d0e72cd29884ae4266
ryanzola <ryanzola@me.com>
2019-07-02 15:41:31 -0400

    Board Function Update
    - Updates the logic for the board render method

--- src/index.js
+++ src/index.js
@@ -12,14 +12,14 @@ function Square({value, onClick}) {
   );
 }
 
-function Board(props) {
-  console.log('board: ', props.board);
+function Board({board, handleClick}) {
+  console.log('board: ', board);
   return (
     <div className="board">
-        {props.board.map((row, i) => (
+        {board.map((row, i) => (
           <div key={i} className="board-row">
             {row.map((col, j) => (
-              <Square key={j} value={col} onClick={() => props.handleClick(i, j)}/>
+              <Square key={j} value={col} onClick={() => handleClick(i, j)}/>
             ))}
           </div>
         ))}
@@ -36,7 +36,7 @@ function Game() {
     Array(3).fill(null),    
   ]);
   const [isXNext, setIsXNext] = useState(true);
-  const status = `Next player: ${ isXNext ? X : O }`;
+  const [status, setStatus] = useState(`Next Player: ${X}`);
   const [patterns, setPatterns] = useState({[X]: 0, [O]: 0})
   const [winner, setWinner] = useState(null)
 
@@ -45,56 +45,55 @@ function handleClick(i, j) {
   nextBoard[i][j] = isXNext ? X : O
   setBoard(nextBoard);
   setIsXNext(!isXNext);
+  setStatus(`Next Player: ${!isXNext ? X : O}`);
 
   // Add the value to the player pattern using bitwise OR
   // @TODO: use setPattern or decouple
   patterns[nextBoard[i][j]] = patterns[nextBoard[i][j]] |= Math.pow(2,(i*3+j));
 }
 
+  useEffect(() => {
+    function checkForWin(patterns) {
+      /* The following array holds all the possible 
+      * winning combinations for the game. Using 
+      * bitwise arithmatic, each cell is represented 
+      * by 2 to the power of the cell's number 
+      * (2^0,2^1,2^2, ... , 2^8). The winning 
+      * combination is the sum of all the numbers 
+      * in the corresponding cells (can also be 
+      * achieved by OR-ing two numbers). For example,
+      * the winning combination for the first row is
+      * 1+2+4 = 7. These patterns are compared with
+      * each player's pattern by using bitwise AND.
+      * if the result of the AND operation is equal 
+      * to the winning pattern, that player is the winner.
+      */
+      const WINNING_PATTERNS = [
+        7, 56, 448,	  // Horizontal
+        73, 146, 292,	// Vertical
+        273, 84			  // Cross
+      ];
 
-
-useEffect(() => {
-  console.log(patterns);
-
-  function checkForWin(patterns) {
-    /* The following array holds all the possible 
-    * winning combinations for the game. Using 
-    * bitwise arithmatic, each cell is represented 
-    * by 2 to the power of the cell's number 
-    * (2^0,2^1,2^2, ... , 2^8). The winning 
-    * combination is the sum of all the numbers 
-    * in the corresponding cells (can also be 
-    * achieved by OR-ing two numbers). For example,
-    * the winning combination for the first row is
-    * 1+2+4 = 7. These patterns are compared with
-    * each player's pattern by using bitwise AND.
-    * if the result of the AND operation is equal 
-    * to the winning pattern, that player is the winner.
-    */
-    const WINNING_PATTERNS = [
-      7, 56, 448,	  // Horizontal
-      73, 146, 292,	// Vertical
-      273, 84			  // Cross
-    ];
-
-    // Loop through all possible winning sets
-    for(let i = 0; i < WINNING_PATTERNS.length; i++) {
-      // Use bitwise AND to determind if the player's score
-      // Holds a winning combination
-      if((WINNING_PATTERNS[i] & patterns[X]) === WINNING_PATTERNS[i])
-        return X;
-      if((WINNING_PATTERNS[i] & patterns[O]) === WINNING_PATTERNS[i])
-        return O;
+      // Loop through all possible winning sets
+      for(let i = 0; i < WINNING_PATTERNS.length; i++) {
+        // Use bitwise AND to determind if the player's score
+        // Holds a winning combination
+        if((WINNING_PATTERNS[i] & patterns[X]) === WINNING_PATTERNS[i])
+          return X;
+        if((WINNING_PATTERNS[i] & patterns[O]) === WINNING_PATTERNS[i])
+          return O;
+      }
+      // No winner
+      return null;
     }
-    // No winner
-    return null;
-  }
 
-  setWinner(checkForWin(patterns))
-  console.log('winner: ', winner);
-}, [patterns, board, winner])
+    setWinner(checkForWin(patterns))
+    console.log('winner: ', winner);
 
-
+    if (winner) {
+      setStatus(`${winner} is the winner!`)
+    }
+  }, [patterns, board, winner])
 
   return (
     <div className="game">
@@ -103,7 +102,6 @@ useEffect(() => {
         <Board board={board} handleClick={handleClick}/>
       </div>
       <div className="game-info">
-        <div>{/* status */}</div>
         <ol>{/* TODO */}</ol>
       </div>
     </div>
@@ -112,4 +110,4 @@ useEffect(() => {
 
 // ========================================
 
-ReactDOM.render(<Game />, document.getElementById('root'));
+ReactDOM.render(<Game />, document.querySelector('#root'));
